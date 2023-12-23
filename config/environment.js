fdf382599873f1474eb49852ccd2e641a89e1964
Ankit <ankitvis609@gmail.com>
2023-12-23 16:30:25 +0530

    Production mode and change url

--- config/environment.js
+++ config/environment.js
@@ -17,15 +17,14 @@ const development = {
   
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    GOOGLE_CALLBACK_URL: "https://placementadda.ankithub.me/user/oauth2callback",
+    GOOGLE_CALLBACK_URL: "https://urlshortener-30l7.onrender.com/user/oauth2callback",
   
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-    GITHUB_CALLBACK_URL:
-      "https://placementadda.ankithub.me/user/auth/github/callback",
+    GITHUB_CALLBACK_URL: "https://urlshortener-30l7.onrender.com/user/auth/github/callback",
   
     mongoDbUrl: process.env.mongoDbUrl,
   };
   
-  module.exports = development;
+  module.exports = production;
   
